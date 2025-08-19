ffc5b13276f01a2a160a8223683b2874d1d77ab9
A-Dev <3xasov@gmail.com>
2025-08-19 15:16:22 +0800

    feat: bootstrap sanity studio

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
 
 export const schemaTypes = [post, author, category, blockContent]
+=======
+export const schemaTypes = []
+>>>>>>> a8f5ca1 (feat: bootstrap sanity studio)
